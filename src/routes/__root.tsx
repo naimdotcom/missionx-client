81851be26af892e5d57dd63222d8977eaf604c03
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2026-01-31 10:21:28 +0600

    feat: add TanStack Router Devtools and enhance app search parameter validation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,32 @@
 import { ScreenSizeIndicator } from "@/components/debug/screen-size-indicator";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet, useRouter } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import "../styles/globals.css";
 
 export const Route = createRootRoute({
-  component: RootComponent,
+  component: () => (
+    <>
+      <RootComponent />
+      <TanStackRouterDevtools />
+    </>
+  ),
+  notFoundComponent: () => {
+    const router = useRouter();
+    return (
+      <div className="flex flex-col items-center justify-center h-screen w-screen gap-4">
+        <h1 className="text-2xl font-bold">404 - Not Found</h1>
+        <p className="text-muted-foreground">
+          The page you are looking for does not exist.
+        </p>
+        <a
+          onClick={() => router.history.back()}
+          className="text-primary hover:underline"
+        >
+          Go back home
+        </a>
+      </div>
+    );
+  },
 });
 
 function RootComponent() {
--- src/routes/_private/apps.tsx
+++ src/routes/_private/apps.tsx
@@ -1,7 +1,18 @@
 import { AppsPage } from "@/features/apps";
 import { createFileRoute } from "@tanstack/react-router";
 
+type AppSearchParams = {
+  page?: number;
+  name?: string;
+  perPage?: number;
+};
 export const Route = createFileRoute("/_private/apps")({
   component: AppsPage,
-  validateSearch: (search) => search as Record<string, any>,
+  validateSearch: (search: Record<string, unknown>): AppSearchParams => {
+    return {
+      page: search.page ? Number(search.page) : undefined,
+      perPage: search.perPage ? Number(search.perPage) : undefined,
+      name: typeof search.name === "string" ? search.name : undefined,
+    };
+  },
 });
