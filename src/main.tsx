e0b842ebbc89dbaff25619a247bd8d9b0c607070
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2026-03-10 13:07:20 +0600

    refactor: remove unused Toaster component from main layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme-provider";
 import { routeTree } from "./routeTree.gen";
 import "./styles/globals.css";
@@ -22,7 +21,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <Toaster />
         <RouterProvider router={router} />
       </ThemeProvider>
     </QueryClientProvider>
